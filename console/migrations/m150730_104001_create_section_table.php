7f6155ba5d57fdb5e1848cba4ad2b1b93289a9e7
resurtm <resurtm@gmail.com>
2015-07-30 16:42:58 +0600

    Add section table.

---
+++ console/migrations/m150730_104001_create_section_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use yii\db\Schema;
+use console\components\Migration;
+
+class m150730_104001_create_section_table extends Migration
+{
+    public function up()
+    {
+        $this->createTable('{{%section}}', [
+            'id' => Schema::TYPE_PK,
+            'title' => Schema::TYPE_STRING . ' NOT NULL',
+            'slug' => Schema::TYPE_STRING . ' NOT NULL',
+
+            'created_at' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'updated_at' => Schema::TYPE_INTEGER . ' NOT NULL',
+        ]);
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%section}}');
+    }
+}
