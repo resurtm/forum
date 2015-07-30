fee89277474a8cdc44e64dd71faee605c8721e00
resurtm <resurtm@gmail.com>
2015-07-30 16:39:38 +0600

    Create custom migration template view file.

---
+++ console/views/migration.php
@@ -0,0 +1,21 @@
+<?php
+/* @var $className string the new migration class name */
+
+echo "<?php\n";
+?>
+
+use yii\db\Schema;
+use console\components\Migration;
+
+class <?= $className ?> extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+
+    }
+}
