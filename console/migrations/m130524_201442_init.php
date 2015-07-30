fbe7a23f4c40e616806e48fbec8091388a269fb0
resurtm <resurtm@gmail.com>
2015-07-30 16:20:50 +0600

    Use common migration component.

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -1,18 +1,12 @@
 <?php
 
 use yii\db\Schema;
-use yii\db\Migration;
+use console\components\Migration;
 
 class m130524_201442_init extends Migration
 {
     public function up()
     {
-        $tableOptions = null;
-        if ($this->db->driverName === 'mysql') {
-            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
-        }
-
         $this->createTable('{{%user}}', [
             'id' => Schema::TYPE_PK,
             'username' => Schema::TYPE_STRING . ' NOT NULL',
@@ -24,7 +18,7 @@ class m130524_201442_init extends Migration
             'status' => Schema::TYPE_SMALLINT . ' NOT NULL DEFAULT 10',
             'created_at' => Schema::TYPE_INTEGER . ' NOT NULL',
             'updated_at' => Schema::TYPE_INTEGER . ' NOT NULL',
-        ], $tableOptions);
+        ]);
     }
 
     public function down()
