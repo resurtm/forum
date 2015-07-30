ea3a28425695254738292384c28d97a09fcf3a5a
resurtm <resurtm@gmail.com>
2015-07-30 18:28:33 +0600

    Add relations to section model.

--- common/models/Section.php
+++ common/models/Section.php
@@ -3,6 +3,7 @@
 namespace common\models;
 
 use yii\db\ActiveRecord;
+use yii\db\ActiveQuery;
 use yii\behaviors\TimestampBehavior;
 use yii\behaviors\SluggableBehavior;
 
@@ -14,6 +15,10 @@ use yii\behaviors\SluggableBehavior;
  *
  * @property integer $created_at
  * @property integer $updated_at
+ *
+ * @property integer $section_id
+ * @property Section $parent
+ * @property Section[] $children
  */
 class Section extends ActiveRecord
 {
@@ -26,5 +31,21 @@ class Section extends ActiveRecord
             TimestampBehavior::className(),
             ['class' => SluggableBehavior::className(), 'attribute' => 'title'],
         ];
+    }
+
+    /**
+     * @return ActiveQuery
+     */
+    public function getParent()
+    {
+        return $this->hasOne(Section::className(), ['id' => 'section_id']);
+    }
+
+    /**
+     * @return ActiveQuery
+     */
+    public function getChildren()
+    {
+        return $this->hasMany(Section::className(), ['section_id' => 'id']);
     }
 }
