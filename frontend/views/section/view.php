381e272775a6b4657223313e439bfafc9416b29e
resurtm <resurtm@gmail.com>
2015-07-31 19:48:18 +0600

    Add breadcrumbs support at the section page.

--- frontend/views/section/view.php
+++ frontend/views/section/view.php
@@ -4,7 +4,14 @@
 
 use yii\helpers\Html;
 
-$this->title = $section->parent === null ? $section->title : $section->parent->title . ' → ' . $section->title;
+if ($section->parent === null) {
+    $this->title = $section->title;
+    $this->params['breadcrumbs'][] = $section->title;
+} else {
+    $this->title = $section->parent->title . ' → ' . $section->title;
+    $this->params['breadcrumbs'][] = ['label' => $section->parent->title, 'url' => $section->parent->getUrl()];
+    $this->params['breadcrumbs'][] = $section->title;
+}
 ?>
 <div class="section-view">
     <h1><?= Html::encode($this->title) ?></h1>
