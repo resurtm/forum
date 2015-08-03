786a8baa089120afda4d73d68bc78b12908bfab0
resurtm <resurtm@gmail.com>
2015-08-03 17:31:27 +0600

    Implement post view action method.

--- frontend/controllers/PostController.php
+++ frontend/controllers/PostController.php
@@ -4,12 +4,24 @@ namespace frontend\controllers;
 
 use yii\web\Controller;
 use yii\web\HttpException;
-use common\models\Section;
+use common\models\Post;
 
-class SectionController extends Controller
+class PostController extends Controller
 {
-    public function actionView($parentSectionId, $parentSectionSlug, $childSectionId, $childSectionId, $id, $slug)
+    public function actionView($id, $slug, $parentSectionId, $parentSectionSlug, $childSectionId, $childSectionSlug)
     {
+        /** @var Post $post */
+        $post = Post::find()
+            ->where(['id' => $id, 'slug' => $slug])
+            ->with(['section', 'section.parent'])
+            ->one();
 
+        if ($post === null ||
+            $post->section->parent->id != $parentSectionId || $post->section->parent->slug != $parentSectionSlug ||
+            $post->section->id != $childSectionId || $post->section->slug != $childSectionSlug) {
+            throw new HttpException(404, 'Cannot find the requested post.');
+        }
+
+        return $this->render('view');
     }
 }
