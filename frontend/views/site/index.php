0fa4d43531ac9253c82e151fe9428943f230fb97
resurtm <resurtm@gmail.com>
2015-08-03 20:07:18 +0600

    Add a new button to create a new post.

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -7,6 +7,12 @@ use yii\helpers\Html;
 $this->title = 'Forum';
 ?>
 <div class="site-index">
+    <div class="row header-actions">
+        <div class="col-sm-3">
+            <?= Html::a('Create a new post', ['post/create'], ['class' => 'btn btn-block btn-success']) ?>
+        </div>
+    </div>
+
     <?php foreach ($sections as $section): ?>
         <?= $this->render('/section/_item', ['section' => $section]) ?>
     <?php endforeach; ?>
