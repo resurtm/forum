6c4df68f70cfac08dd9d197634a3dc7ce46b6780
resurtm <resurtm@gmail.com>
2015-08-05 22:45:02 +0600

    Working draft of nested set tree.

--- common/models/comments/NestedSetComment.php
+++ common/models/comments/NestedSetComment.php
@@ -23,13 +23,16 @@ class NestedSetComment extends Comment
             ->select(['node.id', 'node.text', 'node.level'])
             ->from([static::tableName() . ' node', static::tableName() . ' parent'])
             ->where('node.left BETWEEN parent.{{left}} AND parent.{{right}}')
-            //->andWhere('parent.id = 1')
+//            ->andWhere('parent.id = 1')
             ->andWhere('node.post_id = :pid AND parent.post_id = :pid', [':pid' => $postId])
-            ->orderBy('node.{{left}}');
+            ->orderBy('node.{{left}}, node.created_at')
+            ->groupBy('node.{{id}}');
         $comments = $query->all(static::getDb());
         foreach ($comments as &$comment) {
             $comment['children'] = [];
         }
+//        echo '<pre>';
+//        \yii\helpers\VarDumper::dump($comments);die;
         return $comments;
     }
 
@@ -63,7 +66,26 @@ class NestedSetComment extends Comment
                 $this->right = $maxRight + 2;
                 $this->level = 1;
             } else {
+                $query = new Query();
+                $query
+                    ->select(['{{left}} left', '{{level}} level'])
+                    ->where(['post_id' => $this->post_id, 'id' => $this->parent_id])
+                    ->from(static::tableName());
+                $data = $query->all(static::getDb());
 
+                static::getDb()
+                    ->createCommand('UPDATE ' . static::tableName() . ' SET {{right}} = {{right}} + 2 WHERE {{right}} > :left')
+                    ->bindValue(':left', $data[0]['left'])
+                    ->execute();
+
+                static::getDb()
+                    ->createCommand('UPDATE ' . static::tableName() . ' SET {{left}} = {{left}} + 2 WHERE {{left}} > :left')
+                    ->bindValue(':left', $data[0]['left'])
+                    ->execute();
+
+                $this->left = $data[0]['left'] + 1;
+                $this->right = $data[0]['left'] + 2;
+                $this->level = $data[0]['level'] + 1;
             }
         }
 
